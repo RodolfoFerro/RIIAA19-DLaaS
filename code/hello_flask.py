904b2c03ea0c15ff039183a032d9895f7d7a2d61
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-08-28 05:48:06 -0500

    Uploaded base code.

---
+++ code/hello_flask.py
@@ -0,0 +1,35 @@
+# ===============================================================
+# Author: Rodolfo Ferro
+# Email: ferro@cimat.mx
+# Twitter: @FerroRodolfo
+#
+# ABOUT COPYING OR USING PARTIAL INFORMATION:
+# This script was originally created by Rodolfo Ferro, 
+# for his workshop at RIIAA 2.0. Any explicit usage of 
+# this script or its contents is granted according to 
+# the license provided and its conditions.
+# ===============================================================
+
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def url_principal():
+    return "<h1>Hello world!</h1>"
+
+
+@app.route('/<string:name>')
+def url_name(name):
+    html_code = f"""
+    <h1>Hello, {name}!
+    <p>Welcome to the matrix.</p>
+    """
+    return html_code
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ code/reddit_news_reader.py
@@ -0,0 +1,50 @@
+# ===============================================================
+# Author: Rodolfo Ferro
+# Email: ferro@cimat.mx
+# Twitter: @FerroRodolfo
+#
+# ABOUT COPYING OR USING PARTIAL INFORMATION:
+# This script was originally created by Rodolfo Ferro, 
+# for his workshop at RIIAA 2.0. Any explicit usage of 
+# this script or its contents is granted according to 
+# the license provided and its conditions.
+# ===============================================================
+
+# -*- coding: utf-8 -*-
+
+from unidecode import unidecode
+from pprint import pprint
+import requests
+import json
+
+
+# Consume Reddit API:
+def reddit_headlines(reddit):
+    """Python function to get Reddit news."""
+
+    # Set metadata to make request:
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(reddit)
+    headers = {'User-Agent': '{} Reddit headlines'.format(reddit)}
+
+    # Consume Reddit's API to gather info:
+    html = None  # TODO
+
+    # If status code is OK:
+    if html.status_code == requests.codes.ok:
+        # Parse resonse:
+        # TODO: Build info = parsed info (into utf-8)
+        # pprint(info)
+
+        # Get relevant info:
+        # TODO: Build child = info -> data -> children
+        # TODO: Build list and join it
+        titles = None  # TODO
+    else:
+        titles = None
+
+    return titles
+
+
+if __name__ == "__main__":
+    news = reddit_headlines('artificial')
+    print(news)
